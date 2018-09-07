06abe5df7c324a61cf35fff139521edb60b15f38
fivge <luanxingtong@gmail.com>
2018-09-07 10:29:05 +0800

    feat(users.vue): add users pages

---
+++ src/views/users.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="users">
+    <h1>{{ name }}</h1>
+    <p>hello, {{ name }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+const blog = `http://localhost:3000`;
+
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
+  created() {
+    this.getUser();
+  },
+  mounted() {},
+  methods: {
+    getUser() {
+      axios.get(`${blog}/users/${this.$route.params.name}`).then(r => {
+        this.name = r.data;
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#users {
+  padding: 50px;
+}
+</style>
